c3314db3354ac9118559c06274928e8b3efdf0ba
pol 8139 <pol8139+ac@gmail.com>
2020-04-25 13:27:25 +0900

    Fix about progmem

--- test_remote_r.c
+++ test_remote_r.c
@@ -13,6 +13,8 @@
 #define IROUT 1
 #define LED 3
 
+#define NUM_CODES 3
+
 #define F_CPU 9600000UL
 
 #define sbi(PORT, BIT) PORT |= _BV(BIT)
@@ -20,15 +22,13 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
-// #include <util/delay.h>
 #include <avr/pgmspace.h> 
 // #include "BasicSerial3.h"
 // #include "xitoa.h"
 
-//                                              PLAY                      |<<                       >>|
-// PROGMEM const unsigned int ir_recieve[] = {0x3AC5,     0x09F6,     0xF10E};
-PROGMEM const unsigned char ir_recieve[][4] = {{0x4B, 0x98, 0x3A, 0xC5}, {0x4B, 0x40, 0x09, 0xF6}, {0x4B, 0x40, 0xF1, 0x0E}};
-PROGMEM const unsigned long ir_send[]       =  {0x9E6140BF,               0x9E6120DF,               0x9E61E01F};
+//                                                       PLAY                      |<<                       >>|
+PROGMEM const unsigned char ir_recieve[NUM_CODES][4] = {{0x4B, 0x98, 0x3A, 0xC5}, {0x4B, 0x40, 0x09, 0xF6}, {0x4B, 0x40, 0xF1, 0x0E}};
+PROGMEM const unsigned long ir_send[NUM_CODES]       =  {0x9E6140BF,               0x9E6120DF,               0x9E61E01F};
 
 volatile unsigned char time_26micros = 0;
 volatile unsigned char code_vol[4] = {};
@@ -55,24 +55,21 @@ ISR(PCINT0_vect)
     cbi(PCMSK, PCINT0);
     sei(); // Multiple Interrupt Enable except Pin Change Interrupt
     sbi(PORTB, LED);
-    readIR();
-    // if(!readIR()) {
-    //     TxByte(code_vol[0]);
-    //     TxByte(code_vol[1]);
-    //     TxByte(code_vol[2]);
-    //     TxByte(code_vol[3]);
-    // }
-    // _delay_ms(250);
-    for (char i = 0; i < 55; i++)
-    {
-        delay4500us();
-    }
-    for(char i = 0; i < 4; i++) {
-        if(code_vol[i] != ir_recieve[0][i]) {
-            break;
+    // readIR();
+    if(!readIR()) {
+        // _delay_ms(250);
+        for (char i = 0; i < 55; i++) {
+            delay4500us();
         }
-        if(i == 3) {
-            sendIR(ir_send[0]);
+        for(char i = 0; i < NUM_CODES; i++) {
+            for(char j = 0; j < 4; j++) {
+                if(code_vol[j] != pgm_read_byte(&(ir_recieve[i][j]))) {
+                    break;
+                }
+                if(j == NUM_CODES) {
+                    sendIR(pgm_read_dword(&(ir_send[i])));
+                }
+            }
         }
     }
     initIRIn();
@@ -119,7 +116,6 @@ unsigned char readIR(void)
 {
     //loop_until_bit_is_set(PINB, IRIN); // Wait until the code starts -- the output of IR receiver rodules is active low
     //loop_until_bit_is_clear(PINB, IRIN);
-    // unsigned long code = 0;
     loop_until_bit_is_set(PINB, IRIN);
     unsigned char time = my26Micros();
     loop_until_bit_is_clear(PINB, IRIN);
@@ -142,44 +138,6 @@ unsigned char readIR(void)
             }
         }
     }
-    // code_vol[0] = 0;
-    // code_vol[1] = 0;
-    // code_vol[2] = 0;
-    // code_vol[3] = 0;
-    // for(char i = 31; i < 0; i--) {
-    //     loop_until_bit_is_set(PINB, IRIN);
-    //     time = my26Micros();
-    //     loop_until_bit_is_clear(PINB, IRIN);
-    //     time = my26Micros() - time;
-    //     if(time > 40) {
-    //         code_vol[i >> 3] |= _BV(i & 7);
-    //     }
-    // }
-    // sbi(PORTB, LED);
-    // _delay_ms(250);
-    // cbi(PORTB, LED);
-    // TxByte(code >> 24);
-    // TxByte(code >> 16);
-    // TxByte(code >> 8);
-    // TxByte(code);
-    // TxByte(ir_recieve[0] >> 8);
-    // TxByte(ir_recieve[0]);
-    // TxByte(ir_send[0] >> 8);
-    // TxByte(ir_send[0]);
-    // sendIR(0x4BB6C03F);
-    // for(char i = 0; i < 3; i++) {
-    //     if((unsigned int)(code & 0xFFFF) == ir_recieve[i]) {
-    //         cbi(PORTB, LED);
-    //         _delay_ms(250);
-    //         sbi(PORTB, LED);
-    //         _delay_ms(250);
-    //         sendIR(ir_send[i]);
-    //     }
-    // }
-    // if((unsigned char)code == ir_send[0]) {
-    //     TxByte('a');
-    // }
-    // return code;
     return 0;
 }
 
